b8ef74c9ffea6641df680dd2bb002abfd06a7f2f
Mateusz  Mieciek <mateuszmieciek@Mateuszs-MacBook-Pro.local>
2025-02-15 23:50:24 +0100

    cleanup

---
+++ script.js
@@ -0,0 +1,17 @@
+
+(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1115383');
+
+    
+    //darkmode
+    let checkboxDark = document.querySelector(".darkcheckbox");
+    checkboxDark.addEventListener('change', () => {
+        document.body.classList.toggle('dark');
+        // document.body.classList.toggle('wet');
+ 
+
+     
+    });
